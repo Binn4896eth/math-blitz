0866d9703136e513d19319e5648bffc911f01543
binn4896eth <robin.wijaya2505@gmail.com>
2025-11-25 23:24:59 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,13 @@
 "use client";
 
 import MathGame from "@/components/MathGame";
+import { sdk } from '@farcaster/miniapp-sdk';
+import { useEffect } from 'react';
 
 export default function MathPage() {
+  useEffect(() => {
+        sdk.actions.ready();
+    }, []);
   return (
     <div className="min-h-[calc(100vh-4rem)] flex items-center justify-center p-4">
       <div className="w-full max-w-md">
